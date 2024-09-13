786ed2f406517db6e7e46eab736b7af95aeca07c
Your Name <you@example.com>
2024-09-13 21:23:10 +0530

    add(13/9/24)

--- godaddy/src/components/Header/Header.js
+++ godaddy/src/components/Header/Header.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../asset/styles/Header.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { FaShoppingCart } from "react-icons/fa";
 import logo from "../../asset/images/logo.png";
 import { Link } from 'react-router-dom';
-
+import { FaBars } from 'react-icons/fa'; // Hamburger icon
 
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <div>
     <div className="header">
       <div className="logo">
               <img src={logo}  alt="logo"/>
               </div>
+              <div className="menu-toggle" onClick={toggleMenu}>
+          <FaBars />
+        </div>
             <nav>
-                <ul>
+                <ul className={menuOpen ? 'show' : ''}>
                     <li><a href="#Domains">Domains <FontAwesomeIcon icon={faAngleDown} /></a></li>
                     <li><a href="#Websites and Hosting">Websites and Hosting <FontAwesomeIcon icon={faAngleDown} /></a></li>
                     <li><Link to="/email">Email</Link></li>
--- godaddy/src/components/Home/Home.js
+++ godaddy/src/components/Home/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import "../../asset/styles/HomeMainStyle/Home.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faIndianRupeeSign } from '@fortawesome/free-solid-svg-icons';
 import img1 from "../../asset/images/image1.png";
-
+import black from "../../asset/images/black.jpg";
 
 
 const Home = () => {
   return (
     <div>
       <div className="header2">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />  Type the domain you want
-        <button>Search Domains</button>
+        <input type="text" placeholder="Type the domain you want"/>
+         
+        
+      </div>
+
+      <div className="sd">
+      <button>Search Domains</button>
       </div>
 
      <div className="bt1">
@@ -30,24 +34,27 @@ const Home = () => {
 					Learn More <span className="arrow-icon">→</span>
 				</button>
         </div>
+ 
+    <div className="blackimg">
+      <img src={black} alt="black"/>
+    </div>
 
+    <div className="new">
+      <button>NEW</button>
+    </div>
 
-        <div className="btn1">
-        <button className="find">
-          <button>NEW</button>
-           <h3>AI Suggested Domains</h3>
-           <h2>Find the perfect<br></br>
-               domain and<br></br>
-               business name<br></br>
-               with AI.
-           </h2>
-           <div className="btn2">
-           <button className="try">
-            Try AI Domain Search <span className="arrow-icon">→</span>
-           </button>
-           </div>
-        </button>
-        </div>
+    <div className="suggest">
+      <h4>AI Suggested Domains</h4>
+      <h3>Find the perfect<br></br>
+      domain and<br></br>
+      business name<br></br>
+      with AI.</h3>
+    </div>
+
+    <div className="try_ai_domain">
+      <button>Try AI Domain Search<span className="arrow-icon">→</span></button>
+    </div>
+        
 
       
 
